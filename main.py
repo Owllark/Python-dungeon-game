d0c82e7b26294ec8dc0c2d4a907a61ae96285111
owllark <igrbaran@gmail.com>
2022-11-17 18:45:01 +0300

    Add maze generation function and Ilya Eye Monster

--- main.py
+++ main.py
@@ -2,14 +2,62 @@ import pygame
 import mapModule
 import creatureModule as creature
 
+aimSin = 0;
+aimCos = 0;
+
+def draw_eyes(centerX, centerY, mX, mY, radius):
+    leftX = centerX - 2*radius
+    leftY = centerY
+    rightX = centerX + 2 * radius
+    rightY = centerY
+    global aimSin
+    global aimCos
+    if centerX - mX == 0:
+        aimSin = 0
+    else:
+        aimSin = -(centerX - mX) / ((centerX - mX) ** 2 + (centerY - mY) ** 2) ** (1 / 2)
+    if centerY- mY == 0:
+        aimCos = 1
+    else:
+        aimCos = -(centerY - mY) / ((centerX - mX) ** 2 + (centerY - mY) ** 2) ** (1 / 2)
+
+    pygame.draw.circle(display, (50, 10, 10), (centerX, centerY), radius * 4)
+    pygame.draw.circle(display, white, (leftX, leftY), radius)
+    pygame.draw.circle(display, white, (rightX, rightY), radius)
+    if leftX - mX == 0:
+        sinL = 0
+    else:
+        sinL = -(leftX - mX) / ((leftX - mX) ** 2 + (leftY - mY) ** 2) ** (1 / 2)
+    if leftY - mY == 0:
+        cosL = 1
+    else:
+        cosL = -(leftY - mY) / ((leftX - mX) ** 2 + (leftY - mY) ** 2) ** (1 / 2)
+    pXL = leftX + int((radius / 2) * sinL)
+    pYL = leftY + int((radius / 2) * cosL)
+    pygame.draw.circle(display, black, (pXL, pYL), int(radius / 2))
+
+    if rightX - mX == 0:
+        sinR = 0
+    else:
+        sinR = -(rightX - mX) / ((rightX - mX) ** 2 + (rightY - mY) ** 2) ** (1 / 2)
+    if rightY - mY == 0:
+        cosR = 1
+    else:
+        cosR = -(rightY - mY) / ((rightX - mX) ** 2 + (rightY - mY) ** 2) ** (1 / 2)
+    pXR = rightX + int((radius / 2) * sinR)
+    pYR = rightY + int((radius / 2) * cosR)
+    pygame.draw.circle(display, black, (pXR, pYR), int(radius / 2))
+
+
 pygame.init()
 
 white = (255, 255, 255)
+black = (0, 0, 0)
 purple = (150, 30, 180)
 
-width = 640
-height = 400
-cellSize = 10
+width = 1920
+height = 1080
+cellSize = 40
 
 display = pygame.display.set_mode((width, height))
 pygame.display.update()
@@ -23,12 +71,17 @@ y = height / 2
 pos = (x, y)
 
 lifes = 3
+squareSpeed = 10
+monsterSpeed = 5
+monsterX = width / 2
+monsterY = height / 2
 
-square = creature.MainCharacter(x - 3 * cellSize, y, 1, 2 * cellSize, 3)
+square = creature.MainCharacter(x - 15 * cellSize, y, squareSpeed, cellSize, lifes)
 mainMap = mapModule.Map(width, height, cellSize)
-pressedKey = [False] * 1024;
+pressedKey = [False] * 1024
 mousePressed = False
-
+mX = width / 2
+mY = height / 2
 while not game_over:
     display.fill(white)
     for event in pygame.event.get():
@@ -36,6 +89,8 @@ while not game_over:
             game_over = True
         if event.type == pygame.MOUSEMOTION:
             pos = event.pos
+            mX = pos[0]
+            mY = pos[1]
         if event.type == pygame.MOUSEBUTTONDOWN:
             mousePressed = True
         if event.type == pygame.MOUSEBUTTONUP:
@@ -78,11 +133,19 @@ while not game_over:
     if mousePressed:
         mainMap.set_state(int(pos[0] / cellSize), int(pos[1] / cellSize), 1)
 
-
     rect = pygame.Rect(pos[0] - cellSize / 2, pos[1] - cellSize / 2, cellSize, cellSize)
     mainMap.draw(display)
+    #pygame.draw.rect(display, purple, rect)
+    leftEyeX = mX - 50
+    leftEyeY = mY
+    rightEyeX = mX + 50
+    rightEyeY = mY
+    monsterX += int(aimSin * monsterSpeed)
+    monsterY += int(aimCos * monsterSpeed)
+    draw_eyes(monsterX, monsterY, square.x, square.y, 30)
     square.draw(display)
-    pygame.draw.rect(display, purple, rect)
+    #prightX = int((mX + rightX) / 2)
+    #prightY = int((mY + rightY) / 2)
     pygame.display.update()
     clock.tick(60)
 
--- mapModule.py
+++ mapModule.py
@@ -1,5 +1,10 @@
 import pygame
+import random
 
+class position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 class Map:
     def __init__(self, width, height, cellSize):
         self.width = width
@@ -7,7 +12,13 @@ class Map:
         self.cellSize = cellSize
         self.cellAmountW = int(width / cellSize)
         self.cellAmountH = int(height / cellSize)
-
+        self.field = [False] * self.cellAmountW
+        for i in range(0, self.cellAmountW):
+            self.field[i] = [False] * self.cellAmountH
+        visited = [False] * int(self.cellAmountW / 2)
+        for i in range(0, int(self.cellAmountW / 2)):
+            visited[i] = [False] * int(self.cellAmountH / 2)
+        maze(0 ,0, self.cellAmountW, self.cellAmountH, self.field, visited)
         self.cells = [0] * self.cellAmountW
         for i in range(0, self.cellAmountW):
             self.cells[i] = [0] * self.cellAmountH
@@ -24,7 +35,7 @@ class Map:
             for j in range(0, self.cellAmountH):
                 cell = pygame.Rect(i * self.cellSize, j * self.cellSize, self.cellSize, self.cellSize)
                 activeColor = self.colorDarkGrey
-                if self.cells[i][j] == 1:
+                if self.field[i][j] == True:
                     activeColor = self.colorPurple
 
                 pygame.draw.rect(display, activeColor, cell)
@@ -34,3 +45,28 @@ class Map:
 
     def __del__(self):
         pass
+
+
+def maze(x, y, cellAmountW, cellAmountH, field, visited):
+    visited[x][y] = True
+    field[2 * x + 1][2 * y + 1] = True
+    n = [position(0, 0)] * 4
+    n[0] = position(x - 1, y)
+    n[1] = position(x, y - 1)
+    n[2] = position(x + 1, y)
+    n[3] = position(x, y + 1)
+
+    for i in range(0, 4):
+        idx = random.randint(0, 3)
+        if idx != i:
+            temp = n[idx]
+            n[idx] = n[i]
+            n[i] = temp
+
+    for i in range(0, 4):
+        temp = n[i]
+        if temp.x >= 0 and temp.y >= 0 and temp.x < int(cellAmountW / 2) and temp.y < int(cellAmountH / 2):
+            if not visited[temp.x][temp.y]:
+                field[x*2 + (temp.x - x) + 1][y*2 + (temp.y - y) + 1] = True
+                visited[temp.x][temp.y] = True
+                maze(temp.x, temp.y, cellAmountW, cellAmountH, field, visited)
